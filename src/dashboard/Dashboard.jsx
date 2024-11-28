734d74e75f7f744e2b767358b2ca94f945d060f8
zaidihusain <zaidihusain611@gmail.com>
2024-11-28 21:57:02 +0530

    Correction the issue in Dashboard

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import AddResume from './components/AddResume'
 import { useUser } from '@clerk/clerk-react'
-import GlobalApi from './../../service/GlobalApi';
+import GlobalApi from '../../service/GlobalApi';
 import ResumeCardItem from './components/ResumeCardItem';
 
 function Dashboard() {
